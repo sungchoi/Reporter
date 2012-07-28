ed20412cd34b29131bf9ae506217138fb2d27be1
John Markos O'Neill <john@nhoj.com>
2012-07-28 16:21:31 -0700

    Test validate presence of username.

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   should have_many(:reports)
-
+  should validate_presence_of(:username)
   should 'not allow a user with a short password' do
     u = User.new({
       :password => 'abcde',
