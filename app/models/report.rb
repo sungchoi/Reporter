98d641a1e094ccb760b59d0dffa6dabd121ab473
John Markos O'Neill <john@nhoj.com>
2012-07-28 21:39:21 -0700

    Fix users to work with username instead of email.

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,6 +4,13 @@ class Report < ActiveRecord::Base
   belongs_to :user
   has_many :votes
 
+  validates_numericality_of :latitude, :allow_nil => true,
+    :greater_than_or_equal_to => -90.0,
+    :less_than_or_equal_to    => 90.0
+  validates_numericality_of :longitude, :allow_nil => true,
+    :greater_than_or_equal_to => -180.0,
+    :less_than_or_equal_to    => 180.0
+
   # protected_attributes = {created_at, updated_at, user_id, latest_status, co
   # confirmation_count = number of confirmations
   # inaccuracy_count = number of inaccurate votes
--- test/unit/report_test.rb
+++ test/unit/report_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  should belong_to(:user)
+  should have_many(:votes)
+  [-90.0, -33.3, -45.0, 0.0, 37.5, 45.0, 90.0].each do |lat|
+    should_allow(lat).for(:latitude)
+  end
 end
